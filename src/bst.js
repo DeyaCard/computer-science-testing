32d8d8e4e020678445aa70f54ef47e17393b2563
Deya Card <thedeyacard@gmail.com>
2022-11-06 12:41:13 -0800

    pass tests 2-3

--- src/bst.js
+++ src/bst.js
@@ -6,4 +6,15 @@ export default class BST {
   insertNode(node) {
     this.root = node;
   }
+
+  insert(node) {
+    if (this.root === null) {
+      this.root = node;
+    } else if (this.root.data > node.data) {
+      this.root.left = node;
+    } else if (this.root.data < node.data) {
+      this.root.right = node;
+    }
+  }
 }
+
